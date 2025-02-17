97fadd5345e3733d9ef529d5fee236d5a9198584
Yki-beep <katia0games.ua@gmail.com>
2025-02-17 18:08:22 +0200

    .

---
+++ script.js
@@ -0,0 +1,13 @@
+// const toggleAnswer = (e) => {
+    // e.target.nextElementSibling.classList.toggle('hide');
+// }
+
+// document.querySelectorAll(".myBtn").forEach((btn) => {
+//     btn.addEventListener("click", toggleAnswer)
+// })
+
+document.querySelector("section").addEventListener("click", function(e){
+    if(e.target.classList.contains("myBtn")){
+        e.target.nextElementSibling.classList.toggle('hide');
+    }
+})
