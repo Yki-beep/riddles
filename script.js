c9a631b61e89f8853314ccdfdc93a26921c9839d
Yki-beep <katia0games.ua@gmail.com>
2025-02-17 18:36:51 +0200

    more questions + CSS

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
     // e.target.nextElementSibling.classList.toggle('hide');
 // }
 
-// document.querySelectorAll(".myBtn").forEach((btn) => {
-//     btn.addEventListener("click", toggleAnswer)
-// })
+document.querySelectorAll(".myBtn").forEach((btn) => {
+    btn.addEventListener("click", toggleAnswer)
+})
 
-// document.querySelector("section").addEventListener("click", function(e){
-//     if(e.target.classList.contains("myBtn")){
-//         e.target.nextElementSibling.classList.toggle('hide');
-//     }
-// })
+document.querySelector("section").addEventListener("click", function(e){
+    if(e.target.classList.contains("myBtn")){
+        e.target.nextElementSibling.classList.toggle('hide');
+    }
+})
